6585d19d66a77a8e5b1785522c9f86d0c2e6b5a6
OrdinaryJoys <90075145+OrdinaryJoys@users.noreply.github.com>
2023-02-21 22:06:25 +0800

    Update dl_toolchain.sh

--- toolchain-mipsel/dl_toolchain.sh
+++ toolchain-mipsel/dl_toolchain.sh
@@ -1,57 +1,32 @@
-TOPDIR:=${CURDIR}
-SOURCE_DIR:=$(TOPDIR)/trunk
-TOOLCHAIN_DIR:=$(TOPDIR)/toolchain-mipsel
-TOOLCHAIN_ROOT:=$(TOOLCHAIN_DIR)/toolchain-4.4.x
-TOOLCHAIN_URL:=https://github.com/tsl0922/padavan/releases/download/toolchain/mipsel-linux-uclibc-gcc10.tar.xz
-TEMPLATE_DIR:=$(SOURCE_DIR)/configs/templates
-PRODUCTS:=$(shell ls $(TEMPLATE_DIR) | sed 's/.config//g')
-CONFIG:=$(SOURCE_DIR)/.config
+#!/bin/sh
 
-all: build
+DIR="toolchain-4.4.x"
+DL_URL="https://github.com/tsl0922/padavan/releases/download/toolchain/mipsel-linux-uclibc-gcc10.tar.xz"
 
-toolchain/build:
-	@echo "Building toolchain..."
-	@(cd $(TOOLCHAIN_DIR); \
-		./bootstrap && \
-		./configure --enable-local && \
-		make && \
-		./ct-ng mipsel-linux-uclibc && \
-		./ct-ng build \
-	)
+dl() {
+	[ -z "$1" ] && return
 
-toolchain/clean:
-	@(cd $(TOOLCHAIN_DIR); \
-		if [ -f ct-ng ]; then ./ct-ng distclean; fi; \
-		if [ -f Makefile ]; then make distclean; fi; \
-		if [ -d $(TOOLCHAIN_ROOT) ]; then rm -rf $(TOOLCHAIN_ROOT); fi \
-	)
+	echo "Download toolchain: $1"
+	curl -O -L "${DL_URL}/$1" && \
+	mkdir -p $DIR && \
+	tar -xf "${1}" -C $DIR
+}
 
-toolchain/download:
-	@if [ ! -d $(TOOLCHAIN_ROOT) ]; then \
-		echo "Downloading toolchain..."; \
-		mkdir -p $(TOOLCHAIN_ROOT); \
-		curl -fSsLo- $(TOOLCHAIN_URL) | tar Jx -C $(TOOLCHAIN_ROOT); \
-	fi
+if [ -d $DIR ]; then
+	echo "$DIR exists!"
+	exit
+fi
 
-build: toolchain/download
-	@if [ ! -f $(CONFIG) ]; then \
-		echo "Please run 'make PRODUCT_NAME' to start build!"; \
-		echo "Supported products: $(PRODUCTS)"; \
-		exit 1; \
-	fi
-	$(MAKE) -C $(SOURCE_DIR)
+ARCH="$(uname -m)"
 
-clean:
-	@if [ ! -f $(CONFIG) ]; then \
-		echo "Project config file .config not found! Terminate."; \
-		exit 1; \
-	fi
-	$(MAKE) -C $(SOURCE_DIR) clean
-	@rm -f $(CONFIG)
-
-.PHONY: $(PRODUCTS)
-$(PRODUCTS):
-	cp -f $(TEMPLATE_DIR)/$(@).config $(CONFIG)
-	@echo "CONFIG_CROSS_COMPILER_ROOT=$(TOOLCHAIN_ROOT)" >> $(CONFIG)
-	@echo "CONFIG_CCACHE=y" >> $(CONFIG)
-	@make build
+case $ARCH in
+	aarch64)
+		dl "aarch64_mipsel-linux-uclibc.tar.xz"
+		;;
+	x86_64)
+		dl "mipsel-linux-uclibc.tar.xz"
+		;;
+	*)
+		echo "Unknown ARCH: $ARCH"
+		exit 1
+esac
